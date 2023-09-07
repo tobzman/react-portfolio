2b24fad2776e05cd805e598560a7065cdb262bcc
Tobenna <tvokafor@gmail.com>
2023-09-07 20:58:09 +0100

    updated files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
-import Header from "./components/Header";
-import Navigation from "./components/Navigation";
-import AboutMe from "./components/AboutMe";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
-import Resume from "./components/Resume";
-import Footer from "./components/Footer";
+import Header from "./components/Header.js";
+import Navigation from "./components/Navigation.js";
+import AboutMe from "./components/AboutMe.js";
+import Portfolio from "./components/Portfolio.js";
+import Contact from "./components/ Contact.js"; // Corrected import
+import Resume from "./components/Resume.js";
+import Footer from "./components/Footer.js";
 import "./App.css";
 
 function App() {
   const [activeSection, setActiveSection] = useState("About Me");
 
+  // Define an array of section names
+  const sections = ["About Me", "Portfolio", "Contact", "Resume"];
+
   return (
     <div className="App">
       <Header />
       <Navigation
+        sections={sections} // Pass the sections array as a prop
         activeSection={activeSection}
         setActiveSection={setActiveSection}
       />
